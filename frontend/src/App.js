6a88ae460ac000fc99eeedb336cef2b7f4bb9add
KalekyeRaychelle <kalekyeraychelle@gmail.com>
2025-08-27 16:38:22 +0300

    chore(setup): add initial React project structure
    - Added with placeholder files
    - Included App.js, index.js, and base styles
    
    Closes #23

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import './App.css';
+import ChatPage from './Pages/ChatPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <ChatPage/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
